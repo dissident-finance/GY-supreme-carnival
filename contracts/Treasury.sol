da71ebe81c5e40d1fcab754a39c02c8bbcf0a937
FibrinLab <fibrinlab@gmail.com>
2022-01-27 12:28:47 +0100

    SafeMath Library updated

---
+++ contracts/Treasury.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.7.5;
+
+import "./libraries/SafeMath.sol";
+import "./libraries/SafeERC20.sol";
+
+
+
+contract DissidentTreasury is DissidentAccessControlled, ITreasury {
+    // ======== DEPENDENCIES =========
+
+    using SafeMath for uint256;
+    using SafeERC for IERC20;
+
+    // ======== EVENTS ========
+}
